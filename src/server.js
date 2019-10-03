21c1316b35a4b02b94a93281ffb04af80b5b09a9
Jordy Oliveira <oliveira.jordy03@gmail.com>
2019-10-03 10:14:21 -0300

    cors adicionado

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,7 +9,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-56nia.gcp.mongodb
     useUnifiedTopology: true
 });
 
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
